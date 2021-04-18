38757d20fc9ee3f03c81a3f397ffa3a0f2a132b8
Anthony R. Spyker <anthonyspyker@gmail.com>
2021-04-18 12:14:15 -0400

    wrong api key. deleted

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const URL = `https://randomapi.com/api/${process.env.REFID}?key=${process.env.APIKEY}`;
-const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
+
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
